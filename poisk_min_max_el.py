83219ab155f9c6b296b6593a0d6ea3990ba03933
IVANP225 <53439778+IVANP225@users.noreply.github.com>
2020-06-30 17:17:05 +0500

    1. подготовить данные .csv 2. прочитать данные 3. найти максимум и минимум. Поменять их местами

--- poisk_min_max_el.py
+++ poisk_min_max_el.py
@@ -1,6 +1,17 @@
 import numpy as np
-#y = np.loadtxt('poisk_min_max.csv')
-x = np.zeros((10,10))
-np.savetxt(delimiter=';',fname='zeros.csv',fmt='%1.1i', X=x)
-y = np.loadtxt(delimiter=';',fname='zeros.csv')
-print(y)
+#создали матрицу 10 на 10
+#x = np.random.randint(100, size=(10,10))
+#np.savetxt(delimiter=';',fname='ran.csv',fmt='%1.1i', X=x)
+y = np.loadtxt(delimiter=';',fname='ran.csv')
+
+#поиск минимального и максимального элемента и позиций
+x_minim = np.min(y)
+x_maxim = np.max(y)
+
+tmp_min = np.where(y ==x_minim)
+tmp_max = np.where(y ==x_maxim)
+
+# меняем местами
+y[tmp_min] = x_maxim
+y[tmp_max] = x_minim
+np.savetxt(delimiter=';',fname='ran_izm.csv',fmt='%1.1i', X=y)
